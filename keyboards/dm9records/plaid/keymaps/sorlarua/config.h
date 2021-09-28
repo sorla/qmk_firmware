906572dc2aa9d6b816718c4a67f8b406b44804a5
Alan <alan.bloomer@gmail.com>
2021-09-28 21:12:43 +0100

    Initial one-handed layers added based on Konomu layout

--- keyboards/dm9records/plaid/keymaps/sorlarua/config.h
+++ keyboards/dm9records/plaid/keymaps/sorlarua/config.h
@@ -2,3 +2,5 @@
 
 #define AUTO_SHIFT_MODIFIERS
 #define AUTO_SHIFT_TIMEOUT 120
+
+#define COMBO_TERM 80
--- keyboards/dm9records/plaid/keymaps/sorlarua/keymap.c
+++ keyboards/dm9records/plaid/keymaps/sorlarua/keymap.c
@@ -26,6 +26,18 @@
 #define OS_GUI OSM(MOD_LGUI)
 #define OS_RAL OSM(MOD_RALT)
 
+// Keys for FrogPad layout
+#define BSP_EXT LT(_FEXT,KC_BSPC)
+#define OSYM OSL(_FSYM)
+#define ONUM OSL(_FNUM)
+#define OSFT OSM(MOD_LSFT)
+#define OGUI OSM(MOD_LGUI)
+#define ATAB LCA(KC_TAB)
+#define OALT OSM(MOD_LALT)
+#define OCTL OSM(MOD_LCTL)
+#define CBSP C(KC_BSPC)
+
+// Shortcuts for Teams
 #define TM_QUIT C(S(KC_B))
 #define TM_MUTE C(S(KC_M))
 #define TM_HAND C(S(KC_K))
@@ -41,7 +53,11 @@ uint16_t alt_tab_timer = 0;
 	 _SYMS,
 	 _FUNCTION,
 	 _MOUSE,
-	 _ADJUST
+	 _ADJUST,
+     _FROG,
+     _FSYM,
+     _FNUM,
+     _FEXT
  };
 
  // macro keys
@@ -69,44 +85,78 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
         KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
 		KC_LSFT, LT(6,KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-		KC_LCTL, KC_RALT, KC_LALT, KC_LGUI, MO(_NUMBERS), KC_BSPC, KC_SPC, MO(_SYMS), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+		KC_LCTL, KC_RALT, KC_LALT, KC_LGUI, MO(_NUMBERS), KC_BSPC, KC_SPC, MO(_SYMS), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+        ),
 
 	[_NAV] = LAYOUT_plaid_grid(
 		  KC_F2, KC_HOME,   KC_UP,  KC_END, KC_PGUP, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY,
 		C(KC_S), KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_PSCR,  OS_CTL,  OS_SFT,  OS_ALT,  OS_GUI,
 		C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y), _______, _______, KC_CAPS,  KC_APP, _______,  OS_RAL, _______,
-		_______, _______, _______, _______, _______, ALT_TAB, _______, _______, _______, _______, GO_HOME, _______),
+		_______, _______, _______, _______, _______, ALT_TAB, _______, _______, _______, _______, GO_HOME, _______
+        ),
 
 	[_NUMBERS] = LAYOUT_plaid_grid(
 		_______, _______, _______, _______, _______, _______, _______, _______,   KC_5,    KC_6,    KC_7,    KC_8,
 		 OS_GUI,  OS_ALT,  OS_SFT,  OS_CTL, _______, _______, _______,  KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,
 		_______,  OS_RAL, _______, _______, _______, _______, _______, KC_PLUS,   KC_9, KC_COLN,  KC_DOT, _______,
-		_______, _______, _______, _______, _______, _______,  KC_TAB,    KC_0, KC_ENT, _______, GO_HOME, KC_BSPC),
+		_______, _______, _______, _______, _______, _______,  KC_TAB,    KC_0, KC_ENT, _______, GO_HOME, KC_BSPC
+        ),
 
 	[_SYMS] = LAYOUT_plaid_grid(
 		KC_TILD   ,   KC_LT,  KC_EQL,   KC_GT, KC_CIRC, _______, _______, KC_LBRC, KC_RBRC, KC_NUHS,   UK_AT, KC_COLN,
 		KC_DLR    , UK_DQUO, KC_UNDS, KC_MINS, KC_PLUS, _______, _______, KC_LPRN, KC_RPRN, KC_EXLM, KC_PERC,  UK_PND,
 		ALGR(KC_4), UK_BSLS, UK_PIPE, KC_SLSH, KC_ASTR, _______, _______, KC_LCBR, KC_RCBR, KC_SCLN, KC_COLN, KC_AMPR,
-		   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, GO_HOME, _______),
+		   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, GO_HOME, _______
+        ),
 
 	[_FUNCTION] = LAYOUT_plaid_grid(
 		_______, DM_RSTP, DM_PLY1, DM_PLY2, DM_REC1, _______, _______,  KC_F14,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
 		 OS_GUI,  OS_ALT,  OS_SFT,  OS_CTL, DM_REC2, _______, _______,  KC_F13,   KC_F1,   KC_F2,   KC_F3,   KC_F4,
 		_______, _______, _______, _______, _______, _______, _______,  KC_F15,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, GO_HOME, _______),
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, GO_HOME, _______
+        ),
 
 	[_MOUSE] = LAYOUT_plaid_grid(
 		_______, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, _______, _______,   KC_NO, KC_ACL0, KC_ACL2, KC_BTN3, KC_BTN5,
 		_______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, KC_BTN4, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
 		_______,   KC_NO,  	KC_NO,   KC_NO,   KC_NO, _______, _______, _______, _______, _______, _______, _______,
-		_______, _______, _______, KC_BTN2, KC_BTN1, KC_BTN3, _______, _______, _______, _______, GO_HOME, _______),
+		_______, _______, _______, KC_BTN2, KC_BTN1, KC_BTN3, _______, _______, _______, _______, GO_HOME, _______
+        ),
 
 	[_ADJUST] = LAYOUT_plaid_grid(
-		  RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO, _______,   KC_NO,       KC_NO,        KC_NO,    KC_NO,   KC_NO,  KC_DEL,
-		_______, _______,  MU_MOD,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP, DF(_QWERTY), DF(_COLEMAK),    KC_NO,   KC_NO, _______,
-		_______, _______,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,      MI_OFF,      TERM_ON, TERM_OFF, _______, _______,
-		_______, _______, KC_ASTG, KC_ASDN, KC_ASUP, KC_ASRP, _______,     _______,      _______,  _______, GO_HOME, _______)
+		  RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO, _______,   KC_NO,       KC_NO,        KC_NO,     KC_NO,   KC_NO,  KC_DEL,
+		_______, _______,  MU_MOD,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP, DF(_QWERTY), DF(_COLEMAK), TO(_FROG),   KC_NO, _______,
+		_______, _______,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,      MI_OFF,      TERM_ON,  TERM_OFF, _______, _______,
+		_______, _______, KC_ASTG, KC_ASDN, KC_ASUP, KC_ASRP, _______,     _______,      _______,   _______, GO_HOME, _______
+        ),
 
+    [_FROG] = LAYOUT_plaid_grid(
+         KC_ESC,    KC_F,    KC_D,    KC_N,    KC_I,    KC_G,    KC_G,    KC_I,    KC_N,    KC_D,    KC_F,  KC_ESC,
+        BSP_EXT,    KC_S,    KC_T,    KC_H,    KC_E,    KC_O,    KC_O,    KC_E,    KC_H,    KC_T,    KC_S, BSP_EXT,
+           OSFT,    KC_C,    KC_W,    KC_R,    KC_A,    KC_U,    KC_U,    KC_A,    KC_R,    KC_W,    KC_C,    OSFT,
+        _______, _______, _______,    ONUM,    OSYM,  KC_SPC,  KC_SPC,    OSYM,    ONUM, _______, GO_HOME, _______
+        ),
+
+    [_FSYM] = LAYOUT_plaid_grid(
+       KC_TAB, KC_LBRC, KC_LCBR, KC_LPRN,   KC_AT, KC_CIRC, KC_CIRC,   KC_AT, KC_LPRN, KC_LCBR, KC_LBRC,  KC_TAB,
+      KC_BSLS, KC_COLN, KC_SCLN, KC_QUES, KC_EXLM, KC_SLSH, KC_SLSH, KC_EXLM, KC_QUES, KC_SCLN, KC_COLN, KC_BSLS,
+      KC_TILD, KC_RBRC, KC_RCBR, KC_RPRN, KC_AMPR, KC_ASTR, KC_ASTR, KC_AMPR, KC_RPRN, KC_RCBR, KC_RBRC, KC_TILD,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+
+    [_FNUM] = LAYOUT_plaid_grid(
+       KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,  KC_DOT,  KC_DOT,    KC_9,    KC_8,    KC_7,    KC_6,  KC_EQL,
+      KC_PLUS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_5,    KC_4,    KC_3,    KC_2,    KC_1, KC_PLUS,
+      KC_MINS, KC_HASH,  KC_DLR, KC_PERC,    KC_0, KC_COMM, KC_COMM,    KC_0, KC_PERC,  KC_DLR, KC_HASH, KC_MINS,
+      _______, _______, _______, XXXXXXX, XXXXXXX,    OGUI,    OGUI, XXXXXXX, XXXXXXX,_______, _______, _______
+      ),
+
+    [_FEXT] = LAYOUT_plaid_grid(
+      XXXXXXX, KC_WBAK, KC_HOME,   KC_UP,  KC_END, KC_WFWD, KC_WFWD, KC_HOME,   KC_UP,  KC_END, KC_WBAK, XXXXXXX,
+      XXXXXXX,    ATAB, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL,  KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT,    ATAB, XXXXXXX,
+      XXXXXXX, C(KC_Z), C(KC_X), C(KC_C), C(KC_V),    OALT,    OALT, C(KC_V), C(KC_C), C(KC_X), C(KC_Z), XXXXXXX,
+      _______, _______, _______,    OSFT,    OCTL,    CBSP,    CBSP,    OCTL,    OSFT, _______, _______, _______
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
